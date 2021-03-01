8adae1c4af0fd25d47e25089a4ca28c63325e900
houstonhaynes <h3techsme@gmail.com>
2021-03-01 11:22:20 -0800

    initial commit

---
+++ Program.fs
@@ -0,0 +1,13 @@
+#r "nuget: FSharp.Data.GraphQL.Client, 1.0.7"
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
