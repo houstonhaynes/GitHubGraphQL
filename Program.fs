c9bc3766860be4d4aa979f555964bd378b193a41
houstonhaynes <h3techsme@gmail.com>
2021-03-01 14:08:10 -0800

    removed headerfile

--- Program.fs
+++ Program.fs
@@ -1,10 +1,19 @@
 #r "nuget: FSharp.Data.GraphQL.Client, 1.0.7"
 
+#if INTERACTIVE
+#load @"./SnowflaqeClient/GitHubGQL.GraphqlClient.fs"
+#endif
+
+open GitHubGQL
 open System
 
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
+
+let query = getRepoCountForDateRange.query
+
+
 
 [<EntryPoint>]
 let main argv =
